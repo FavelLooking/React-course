92f1df83ac6cebfb1d078c0bf71d6445ef2c5b92
FavelLooking <intwestpol@gmail.com>
2024-07-15 02:13:03 +0400

    feat: add tests

--- src/components/ErrorBoundary/ErrorBoundary.tes.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tes.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import ErrorBoundary from './ErrorBoundary';
 
-const ChildComponent = () => {
+export const ChildComponent = () => {
   throw new Error('Test error');
 };
 
