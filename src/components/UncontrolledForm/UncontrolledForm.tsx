6d6a9c4bbbb47fc10d91bd18966c0e43223a2729
FavelLooking <intwestpol@gmail.com>
2024-08-18 02:45:15 +0400

    feat: add validation for uncontrolled form for all except password and file

--- src/components/UncontrolledForm/UncontrolledForm.tsx
+++ src/components/UncontrolledForm/UncontrolledForm.tsx
@@ -1,5 +1,5 @@
 import styles from './UncontrolledForm.module.scss';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
@@ -19,6 +19,8 @@ export function UncontrolledForm() {
   const inputFile = useRef<HTMLInputElement>(null);
   const inputCountry = useRef<HTMLInputElement>(null);
 
+  const [formErrors, setFormErrors] = useState<{ [key: string]: string }>({});
+
   const countries = useSelector(
     (state: RootState) => state.dataSlice.countries,
   );
@@ -37,23 +39,26 @@ export function UncontrolledForm() {
       confirmPassword: inputPasswordValidation.current?.value,
       gender: inputGender.current?.value,
       isChecked: inputCheckbox.current?.checked as boolean,
-      file: inputFile.current?.files?.[0].name,
+      file: inputFile.current?.files?.[0]?.name,
       country: inputCountry.current?.value,
     };
 
     try {
       await validationSchema.validate(formData, { abortEarly: false });
       console.log('Valid data:', formData);
+      dispatch(save(formData));
+      navigate('/');
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
+        const errors: { [key: string]: string } = {};
         err.inner.forEach((error) => {
-          console.error(error.path, error.message);
+          if (error.path) {
+            errors[error.path] = error.message;
+          }
         });
+        setFormErrors(errors);
       }
     }
-
-    dispatch(save(formData));
-    navigate('/');
   };
 
   return (
@@ -64,50 +69,66 @@ export function UncontrolledForm() {
           Name:
           <input type="text" ref={inputName} name="username" />
         </label>
-        <br />
+        {(formErrors.userName && (
+          <p className={styles['error']}>{formErrors.userName}</p>
+        )) || <p></p>}
         <label>
           Age:
-          <input type="number" ref={inputAge} name="age" />
+          <input ref={inputAge} name="age" />
         </label>
-        <br />
+        {(formErrors.age && (
+          <p className={styles['error']}>{formErrors.age}</p>
+        )) || <p></p>}
         <label>
           Email:
-          <input type="email" ref={inputEmail} name="email" />
+          <input ref={inputEmail} name="email" />
         </label>
-        <br />
+        {(formErrors.email && (
+          <p className={styles['error']}>{formErrors.email}</p>
+        )) || <p></p>}
         <label>
           Password:
           <input type="password" ref={inputPassword} name="password" />
         </label>
-        <br />
+        {(formErrors.password && (
+          <p className={styles['error']}>{formErrors.password}</p>
+        )) || <p></p>}
         <label>
-          Password:
+          Confirm Password:
           <input
             type="password"
             ref={inputPasswordValidation}
             name="confirmPassword"
           />
         </label>
-        <br />
+        {(formErrors.confirmPassword && (
+          <p className={styles['error']}>{formErrors.confirmPassword}</p>
+        )) || <p></p>}
         <label>
           Select gender:
-          <select ref={inputGender}>
-            <option>Male</option>
-            <option>Female</option>
+          <select ref={inputGender} name="gender">
+            <option value="">Select Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
           </select>
-          <br />
         </label>
-        <br />
+        {(formErrors.gender && (
+          <p className={styles['error']}>{formErrors.gender}</p>
+        )) || <p></p>}
         <label>
           Terms and Conditions:
           <input type="checkbox" ref={inputCheckbox} name="terms" />
         </label>
-        <br />
+        {(formErrors.isChecked && (
+          <p className={styles['error']}>{formErrors.isChecked}</p>
+        )) || <p></p>}
         <label>
           Upload picture:
           <input type="file" ref={inputFile} name="fileUpload" />
         </label>
-        <br />
+        {(formErrors.file && (
+          <p className={styles['error']}>{formErrors.file}</p>
+        )) || <p></p>}
         <label htmlFor="country">
           Choose your country:
           <input
@@ -119,12 +140,15 @@ export function UncontrolledForm() {
           />
           <datalist id="countries">
             {countries.map((country) => (
-              <option key={country}>{country}</option>
+              <option key={country} value={country}>
+                {country}
+              </option>
             ))}
           </datalist>
-          <br />
         </label>
-        <br />
+        {(formErrors.country && (
+          <p className={styles['error']}>{formErrors.country}</p>
+        )) || <p></p>}
         <button type="submit">Submit</button>
       </form>
     </div>
